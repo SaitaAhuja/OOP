81e369af8bfeec20ec5e9ce5532cd978e3c4fdfc
SaitaAhuja <160378533+SaitaAhuja@users.noreply.github.com>
2026-03-30 18:32:06 +0500

    Added Abstraction.cpp

--- Abstraction/Abstraction.cpp
+++ Abstraction/Abstraction.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
-
 using namespace std;
-
 class Shape {
 public:
     virtual void draw() = 0;
 };
-
 class Circle : public Shape {
 public:
     void draw() override { cout << "Drawing a Circle ◯" << endl; }
 };
-
 class Square : public Shape {
 public:
     void draw() override { cout << "Drawing a Square □" << endl; }
