84f7bff39adfe818c9d33ea24596321601cd20b8
SaitaAhuja <160378533+SaitaAhuja@users.noreply.github.com>
2026-05-27 23:50:45 +0500

    added oop practiceee

--- 1D Dynamic Array and Queue/Q1.cpp
+++ 1D Dynamic Array and Queue/Q1.cpp
@@ -1,77 +1,58 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-template<typename T1, typename T2>
-class Calculator{
-    private:
-       T1 num1;
-       T2 num2;
-    public:
-     Calculator(T1 a,T2 b){
-       num1=a;
-       num2=b;
-   }
-    void add(){
-       cout<<"Addition: "<<(num1+num2)<<endl;
-   }
-    void subtract(){
-      cout<<"Subtraction: "<<(num1-num2)<<endl;
-   }
-    void multiply(){
-     cout<<"Multiplication: "<<(num1*num2)<<endl;
-   }
-    void divide(){
-     if(num2!=0)
-      cout<<"Division: "<<(num1/num2)<<endl;
-      else
-      cout<<"Error: Division by zero"<<endl;
-   }
+
+class DynamicArray {
+private:
+    int* data;
+    int size;
+
+public:
+  
+    DynamicArray() {
+        size = 0;
+        data = nullptr;
+    }
+
+
+    DynamicArray(int n) {
+        size = n;
+        data = new int[size];
+        for (int i = 0; i < size; i++) {
+            data[i] = 0;
+        }
+    }
+
+
+    DynamicArray(const DynamicArray& other) {
+        size = other.size;
+        data = new int[size];
+        for (int i = 0; i < size; i++) {
+            data[i] = other.data[i];
+        }
+    }
+
+   
+    ~DynamicArray() {
+        delete[] data;
+    }
+
+    
+    int& operator[](int index) {
+        return data[index];
+    }
+
+    int getSize() const {
+        return size;
+    }
 };
-  int main(){
-   cout<<"\nSame Datatype"<<endl;
-   Calculator<int,int> obj1(10,5);
-   obj1.add();
-   obj1.subtract();
-   obj1.multiply();
-   obj1.divide();
+int main() {
+    DynamicArray a(3);
+    a[0] = 10;
 
-   cout<<"\nDifferent Datatypes"<<endl;
-   Calculator<int,double> obj2(10,2.5);
-   obj2.add();
-   obj2.subtract();
-   obj2.multiply();
-   obj2.divide();
-  return 0;
+    DynamicArray b = a; 
+    b[0] = 20;
+
+    cout << a[0];
+    return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
