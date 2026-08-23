f4f7c83e663cd2f96710c9cd2e2518d53950dcad
SaitaAhuja <160378533+SaitaAhuja@users.noreply.github.com>
2026-08-23 22:54:39 +0500

    Function_Overloading.cpp
    Removed unnecessary blank lines in the Math class.

--- Function Overloading/Function_Overloading.cpp
+++ Function Overloading/Function_Overloading.cpp
@@ -5,12 +5,10 @@ public:
     int add(int a, int b) {
         return a + b;
     }
- 
     double add(double a, double b) {
         return a + b;
     }
 };
-
 int main() {
     Math m;
     cout << m.add(2, 3) << endl;
