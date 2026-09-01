def4384dad52ed0b1c5a64920cdacb884d812798
SaitaAhuja <160378533+SaitaAhuja@users.noreply.github.com>
2026-09-01 23:26:29 +0500

    Person and Student classes
    Refactor code formatting and remove unnecessary whitespace.

--- Pillars of Oop/code.cpp
+++ Pillars of Oop/code.cpp
@@ -1,41 +1,34 @@
 #include <iostream>
 using namespace std;
-
 // Base class
 class Person {
 protected:
     string name;
-
 public:
     // Constructor
     Person(string n) {
         name = n;
     }
-
     // Virtual function (for polymorphism)
     virtual void display() {
         cout << "Name: " << name << endl;
     }
 };
-
 // Derived class
 class Student : public Person {
 private:
     int marks;
-
 public:
     // Constructor
     Student(string n, int m) : Person(n) {
         marks = m;
     }
-
     // Function overriding
     void display() {
         cout << "Name: " << name << endl;
         cout << "Marks: " << marks << endl;
     }
 };
-
 int main() {
     // Object of base class
     Person p1("Ali");
