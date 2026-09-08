c36e0252134b0039a9a93c25895b18bfd81cbef5
SaitaAhuja <160378533+SaitaAhuja@users.noreply.github.com>
2026-09-08 23:57:55 +0500

    Clarity and formatting
    Fix formatting issues and remove unnecessary braces.

--- pointer/ptr.cpp
+++ pointer/ptr.cpp
@@ -8,15 +8,14 @@ int main(){
 	int *ptr = array;
 	for(int i = 0; i < n ;i ++){
 		cin>>*(ptr+i);
-	}
+}
 	for(int i = 0; i < n ;i ++){
 		cout<<*(ptr+i);
-	}
+}
 	int big = *ptr;
 	for(int i = 0;i<n;i++){
 		if(*(ptr+i)>big){
-			big = *(ptr+i);
-		}
+			big = *(ptr+i);		}
 }
 cout<<big<<endl;
 delete[] array;
